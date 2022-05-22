786929d8688be0a9b6d92ca97385a26579950348
Jaerang Lim <sonwell09863@gmail.com>
2022-05-22 16:17:34 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"))
+    .json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(()=>{
+    getMovies()
+  },[]);
+  console.log(movies);
+  return <div>
+    {loading ? <h1>Loading...</h1> : 
+    <div>{movies.map(movie => 
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image} />
+        <h2>{movie.title} ({movie.year})</h2>
+        <p>{movie.summary}</p>
+        {(movie.hasOwnProperty("genres") ? <ul>
+          {movie.genres.map( (genre) => <li key={genre}>{genre}</li> )}
+        </ul> : null)}
+      </div>
+    )}</div>}
+  </div>;
+}
+
+export default App;
