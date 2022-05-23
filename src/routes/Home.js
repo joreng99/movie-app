3ce4e007b6d890fbdd585c7bd23afbd1dfd56550
Jaerang Lim <sonwell09863@gmail.com>
2022-05-23 20:52:07 +0900

    CSS: background touch

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
-    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"))
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"))
     .json();
     setMovies(json.data.movies);
     setLoading(false);
